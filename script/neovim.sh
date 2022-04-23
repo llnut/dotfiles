57ce73a5b1762ac17e71042e2500fcd6ab14b08d
jcsora <jcsora@outlook.com>
2022-04-23 23:41:53 +0800

    chore: display installation details

--- script/neovim.sh
+++ script/neovim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 echo "Installing neovim-nightly..."
 
@@ -7,7 +8,7 @@ BIN_PATH="$CURDIR/../bin"
 FILE="nvim-linux64.tar.gz"
 
 rm -rf $BIN_PATH/nvim-linux64
-curl -o $BIN_PATH/$FILE --proto '=https' --tlsv1.2 -sSLf https://github.com/neovim/neovim/releases/download/nightly/$FILE
+curl -o $BIN_PATH/$FILE --proto '=https' --tlsv1.2 -SLf https://github.com/neovim/neovim/releases/download/nightly/$FILE
 cd $BIN_PATH && tar zxf $FILE
 ln -sf $BIN_PATH/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
 rm -f $BIN_PATH/$FILE
