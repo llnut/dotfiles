a99ba116893956b17ce3e3b9b4758b2addf2a404
jcsora <jcsora@outlook.com>
2022-06-10 10:12:41 +0800

    feat: back up old files before upgrading

--- script/lldb.sh
+++ script/lldb.sh
@@ -7,7 +7,8 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BIN_PATH="$CURDIR/../bin" && mkdir -p $BIN_PATH
 FILE="codelldb-x86_64-linux.vsix"
 
-rm -rf $BIN_PATH/codelldb-x86_64-linux
+source ./util.sh
+bk $BIN_PATH/codelldb-x86_64-linux
 curl -o $BIN_PATH/$FILE --proto '=https' --tlsv1.2 -SLf https://github.com/vadimcn/vscode-lldb/releases/download/v1.7.0/$FILE
 cd $BIN_PATH && unzip $FILE -d codelldb-x86_64-linux
 rm -f $BIN_PATH/$FILE
--- script/neovim.sh
+++ script/neovim.sh
@@ -7,7 +7,8 @@ CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BIN_PATH="$CURDIR/../bin" && mkdir -p $BIN_PATH && mkdir -p $HOME/.local/bin
 FILE="nvim-linux64.tar.gz"
 
-rm -rf $BIN_PATH/nvim-linux64
+source ./util.sh
+bk $BIN_PATH/nvim-linux64
 curl -o $BIN_PATH/$FILE --proto '=https' --tlsv1.2 -SLf https://github.com/neovim/neovim/releases/download/nightly/$FILE
 cd $BIN_PATH && tar zxf $FILE
 ln -sf $BIN_PATH/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
