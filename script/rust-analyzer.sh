164e32d5d9b7b63087a7f0cd7babe61f32d9af34
jcsora <jcsora@outlook.com>
2022-06-28 18:40:56 +0800

    chore: do some edit

--- script/rust-analyzer.sh
+++ script/rust-analyzer.sh
@@ -2,11 +2,12 @@
 set -x
 
 echo "Installing rust-analyzer..."
-CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BIN_PATH="$CURDIR/../bin" && mkdir -p $BIN_PATH && mkdir -p $HOME/.local/bin
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+BIN_PATH="$CUR_DIR/../bin" && mkdir -p $BIN_PATH && mkdir -p $HOME/.local/bin
 FILE="rust-analyzer-x86_64-unknown-linux-gnu.gz"
 
-source ./util.sh
+source $SCRIPT_DIR/util.sh
 bk $BIN_PATH/rust-analyzer-x86_64-unknown-linux-gnu
 curl -o $BIN_PATH/$FILE --proto '=https' --tlsv1.2 -SLf https://github.com/rust-lang/rust-analyzer/releases/download/nightly/$FILE
 cd $BIN_PATH && gzip -d $FILE
