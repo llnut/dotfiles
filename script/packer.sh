2c931a4bd322acea0c55097c0aa7038da445a7e3
jcsora <jcsora@outlook.com>
2022-08-12 18:31:07 +0800

    chore: do some edit

--- script/packer.sh
+++ script/packer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -x
 
 echo "Installing packer.nvim..."
 
-SCRIPT_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
-BIN_DIR=$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim && mkdir -p $BIN_DIR
+SCRIPT_PATH="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+source $SCRIPT_PATH/util.sh
 
-source $SCRIPT_DIR/util.sh
-bk $BIN_DIR
-git clone --depth 1 https://github.com/wbthomason/packer.nvim $BIN_DIR
+SAVE_PATH="$HOME/.local/share/nvim/site/pack/packer/start"
+SAVE_DIR="packer.nvim"
+
+backup $SAVE_PATH/$SAVE_DIR
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $SAVE_PATH/$SAVE_DIR
