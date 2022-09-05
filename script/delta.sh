21aeb8160b9743851a9a018127f058e094d349ca
jcsora <jcsora@outlook.com>
2022-09-05 10:38:23 +0800

    feat: gitconfig with delta

--- script/delta.sh
+++ script/delta.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Installing vscode-lldb..."
+echo "Installing delta..."
 
 SCRIPT_PATH="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_PATH/util.sh
 
-SAVE_DIR="codelldb"
+SAVE_DIR="delta"
 SAVE_FILE_PREFIX="$SAVE_DIR"
-SAVE_FILE_SUFFIX=".vsix"
-BIN_PATH="$SAVE_PATH/$SAVE_DIR/extension/adapter"
-BIN=("codelldb")
+SAVE_FILE_SUFFIX=".tar.gz"
+BIN_PATH="$SAVE_PATH/$SAVE_DIR"
+BIN=("delta")
 
 backup $SAVE_PATH/$SAVE_DIR
 
-LATEST_URL="https://github.com/vadimcn/vscode-lldb/releases/latest"
+LATEST_URL="https://github.com/dandavison/delta/releases/latest"
 LATEST_URL=`github_latest_url "$LATEST_URL"`
 LATEST_TAG=`echo $LATEST_URL | awk -F '/' '{print $NF}'`
-REMOTE_FILE_PREFIX="codelldb-x86_64-linux"
+REMOTE_FILE_PREFIX="delta-${LATEST_TAG}-x86_64-unknown-linux-gnu"
 download "$SAVE_PATH/$SAVE_FILE_PREFIX$SAVE_FILE_SUFFIX" "$LATEST_URL/$REMOTE_FILE_PREFIX$SAVE_FILE_SUFFIX"
 
 cd $SAVE_PATH && wrap_decompress $SAVE_DIR $SAVE_FILE_PREFIX$SAVE_FILE_SUFFIX
