33c2a9809a6294a15b9e7d2edda76a1c6f09463f
jcsora <jcsora@outlook.com>
2022-12-09 10:42:42 +0800

    fix clash-premium config && update asusd config

--- script/clash-premium.sh
+++ script/clash-premium.sh
@@ -13,7 +13,6 @@ BIN=("clash-premium")
 
 LATEST_URL="https://release.dreamacro.workers.dev/latest/"
 LATEST_TAG="latest"
-[ -f "$SAVE_PATH/$SAVE_DIR/.$LATEST_TAG" ] && echo "Installation successful." && exit 0
 REMOTE_FILE_PREFIX="clash-linux-amd64-v3-${LATEST_TAG}"
 download "$SAVE_PATH/$SAVE_FILE_PREFIX$SAVE_FILE_SUFFIX" "$LATEST_URL/$REMOTE_FILE_PREFIX$SAVE_FILE_SUFFIX"
 
@@ -21,7 +20,6 @@ backup $SAVE_PATH/$SAVE_DIR
 
 cd $SAVE_PATH && wrap_decompress $SAVE_DIR $SAVE_FILE_PREFIX$SAVE_FILE_SUFFIX
 circulate_ln "$BIN_PATH" "${BIN[*]}" "$LOCAL_BIN_PATH"
-touch $SAVE_DIR/.$LATEST_TAG
 rm -f $SAVE_PATH/$SAVE_FILE_PREFIX$SAVE_FILE_SUFFIX
 echo "Installation successful."
 
