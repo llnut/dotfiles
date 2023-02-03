79e3e074bdf923b77d573596c00bdba715b8df07
jcsora <jcsora@outlook.com>
2023-02-03 11:11:17 +0800

    hyperfine script && sync latest nvim plugin config

--- config/.config/nvim/lua/plugin_config/gruvbox.lua
+++ config/.config/nvim/lua/plugin_config/gruvbox.lua
@@ -2,12 +2,16 @@ require("gruvbox").setup({
   undercurl = true,
   underline = true,
   bold = true,
-  italic = true, -- will make italic comments and special strings
-  inverse = true, -- invert background for search, diffs, statuslines and errors
+  italic = true,
+  strikethrough = true,
   invert_selection = false,
   invert_signs = false,
   invert_tabline = false,
   invert_intend_guides = false,
-  contrast = "soft", -- can be "hard" or "soft"
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
   overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
 })
